b3e20457029b62e78e1029438870bb65e79fe5b6
Shamsudeen Ibrahim <shamsain3@gmail.com>
2025-02-11 00:13:55 +0100

    added initial project setup

---
+++ cmam/src/main/java/com/cmam/cmam/CmamApplication.java
@@ -0,0 +1,13 @@
+package com.cmam.cmam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmamApplication.class, args);
+	}
+
+}
---
+++ cmam/src/test/java/com/cmam/cmam/CmamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmam.cmam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
